9a73cda31deaabcd273be7c7c1e4f45159d904aa
NASH <nash@NASHs-iMac.local>
2023-10-11 16:32:05 -0300

    add: titulo cv

--- src/app/pdf/pdf.component.ts
+++ src/app/pdf/pdf.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../Class/User';
 
 import pdfMake from 'pdfmake/build/pdfmake';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
@@ -9,14 +10,43 @@ pdfMake.vfs = pdfFonts.pdfMake.vfs;
   templateUrl: './pdf.component.html',
   styleUrls: ['./pdf.component.scss']
 })
-export class PdfComponent {
+
+
+export class PdfComponent{
+
   createPdf(){
+    const usuario = new User('Felipe alonso perez gonzalez','Ingeniero en Informatica','felipe@gmail.com','+56932443209');
+
     const pdfDefinition: any = {
       content: [
         {
-          text: 'Hoaaala Mundo'
+            text: usuario.profession,style: 'defaultStyle'
+        },
+        {
+          columns: [
+            {
+              width: 340,
+              text: usuario.name, style: 'titleStyle'
+            },
+            {
+              width: 220,
+              ul: [
+                {text: usuario.phone},
+                {text: usuario.email}
+              ]
+            }
+          ],
         }
-      ]
+      ],
+      styles:{
+        defaultStyle:{
+          fontSize: 10,
+          color: 'gray'
+        },
+        titleStyle:{
+          fontSize: 24,
+        }
+      }
     }
     const pdf = pdfMake.createPdf(pdfDefinition);
     pdf.open();
